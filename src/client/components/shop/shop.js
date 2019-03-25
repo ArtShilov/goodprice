5a8e5d05e8d8f5f95ad24beaed2d34c0ac4bd61b
le0n229 <leon229@yandex.ru>
2019-03-25 15:48:11 +0300

    for heroku

---
+++ src/client/components/shop/shop.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './shop.css';
+import moment from 'moment';
+
+export default class Shop extends Component {
+  render() {
+    moment.locale('ru');
+    return (
+    <div>
+      <a href={this.props.link}>{this.props.name}</a>
+      <div>Цена: {this.props.price} руб.</div>
+      <div>Наличие: {this.props.presence}</div>
+      <div>Дата обновления: {moment(Date.parse(this.props.lastUpdate)).format('llll')}
+      </div>
+    </div>
+    );
+  }
+}
