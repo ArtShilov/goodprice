32418eae79b50b79f119e5034a587d921472e884
le0n229 <leon229@yandex.ru>
2019-03-28 14:33:29 +0300

    fixed price and presence update

--- src/client/components/breadcrumbs/breadcrumbs.jsx
+++ src/client/components/breadcrumbs/breadcrumbs.jsx
@@ -74,37 +74,9 @@ class Breadcrumbs extends Component {
   }
 }
 
-<<<<<<< HEAD
-// < !--Button trigger modal-- >
-//   <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
-//     Запустить модальное окно
-// </button>
-
-//   <!--Modal -->
-//     <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-//       <div class="modal-dialog modal-dialog-centered" role="document">
-//         <div class="modal-content">
-//           <div class="modal-header">
-//             <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-//             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-//               <span aria-hidden="true">&times;</span>
-//             </button>
-//           </div>
-//           <div class="modal-body">
-//             ...
-//       </div>
-//           <div class="modal-footer">
-//             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-//             <button type="button" class="btn btn-primary">Save changes</button>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-=======
 
 const BreadcrumbsApp = connect(
   mapStateToProps,
   mapDispatchToProps
 )(Breadcrumbs);
 export default BreadcrumbsApp;
->>>>>>> 5f7c53cd62d0d4d120a1b90ce8762fa4ea1550a2
