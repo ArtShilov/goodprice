72e1eac962c0164034bf90a8b3ca15c7f1356a5e
beshunmo <v.khvan@outlook.com>
2019-03-28 19:03:55 +0300

    fixes pagination

--- src/client/components/home-page/home-page.jsx
+++ src/client/components/home-page/home-page.jsx
@@ -27,7 +27,9 @@ const mapDispatchToProps = dispatch => bindActionCreators({
 
 class HomePage extends Component {
   state = {
-    shops: []
+    shops: [],
+    currentPage: 1,
+    productsPerPage: 6
   }
 
   componentDidMount() {
@@ -45,9 +47,25 @@ class HomePage extends Component {
     localStorage.setItem('cart', JSON.stringify(cart.concat(item)));
   }
 
+  handleClickPagination = (event) => {
+    console.log('TCL: HomePage -> handleClickPagination -> event', event.target.id);
+    this.setState({
+      currentPage: Number(event.target.id)
+    });
+  }
+
   viewCards = () => {
+    const { currentPage, productsPerPage } = this.state;
+    console.log('TCL: HomePage -> viewCards -> currentPage', currentPage);
     const { showProducts1 } = this.props;
-    return showProducts1.map(item => (
+    console.log('TCL: HomePage -> viewCards -> showProducts1', showProducts1);
+    const indexOfLastproduct = currentPage * productsPerPage;
+    console.log('TCL: HomePage -> viewCards -> indexOfLastproduct', indexOfLastproduct);
+    const indexOfFirstproduct = indexOfLastproduct - productsPerPage;
+    console.log('TCL: HomePage -> viewCards -> indexOfFirstproduct', indexOfFirstproduct);
+    const currentproducts = showProducts1.slice(indexOfFirstproduct, indexOfLastproduct);
+
+    return currentproducts.map(item => (
       <CardProduct
         onClick={a => this.handleClickCardBtn(a)}
         key={item._id}
@@ -144,6 +162,12 @@ class HomePage extends Component {
   // };
 
   render() {
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(this.props.showProducts1.length / this.state.productsPerPage); i++) {
+      pageNumbers.push(i);
+    }
+
+    const renderPageNumbers = pageNumbers.map(number => (<a onClick={this.handleClickPagination} className="paginator__item js-paginator__btn" data-type="btn" id={number}>{number}</a>));
     return (
       <div className='home-page'>
         {/* <button onClick={this.test}>test</button> */}
@@ -176,11 +200,8 @@ class HomePage extends Component {
                     <path d="M23.2 31.467c0.533 0 1.067-0.133 1.467-0.533 0.8-0.8 0.8-2 0-2.8l-12.4-12.4 12.4-12.4c0.8-0.8 0.8-2 0-2.8s-2-0.8-2.8 0l-15.2 15.2 15.2 15.2c0.4 0.4 0.8 0.533 1.333 0.533z"></path>
                   </svg>
                 </a>
-                <a href="/catalog/moloko-syr-yaytsa&amp;page=2&amp;sort=rate_desc" className="paginator__item js-paginator__btn" data-type="btn">1</a>
-                <a href="/catalog/moloko-syr-yaytsa?page=2&amp;page=2&amp;sort=rate_desc" className="paginator__item js-paginator__btn _active" data-type="btn">2</a>
-                <a href="/catalog/moloko-syr-yaytsa?page=3&amp;page=2&amp;sort=rate_desc" className="paginator__item js-paginator__btn" data-type="btn">3</a>
-                <a href="/catalog/moloko-syr-yaytsa?page=4&amp;page=2&amp;sort=rate_desc" className="paginator__item js-paginator__btn" data-type="btn">4</a>
-                <a href="http://www.perekrestok.ru/catalog/moloko-syr-yaytsa?page=2&amp;page=2&amp;sort=rate_desc" className="paginator__item js-paginator__next" rel="next">
+                {renderPageNumbers}
+                <a onClick={this.handleClickPagination} className="paginator__item js-paginator__next" rel="next">
                   <svg id="svg__next" viewBox="0 0 32 32" width="100%" height="100%">
                     <title>next</title>
                     <path d="M8.667 31.467c-0.533 0-1.067-0.133-1.467-0.533-0.8-0.8-0.8-2 0-2.8l12.4-12.4-12.4-12.4c-0.8-0.667-0.8-2 0-2.8s2.133-0.8 2.933 0l15.2 15.2-15.2 15.2c-0.4 0.4-0.933 0.533-1.467 0.533z"></path>
